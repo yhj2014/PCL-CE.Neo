7d2cd1e47b03fba8733492b430b9f87ef6bc0a3c
yhj2014 <18172759506@163.com>
2026-05-13 11:50:22 +0000

    chore: 添加 .trae 和文档文件到 .gitignore，移除已跟踪的文件

--- PCL-CE.Neo.Core.Abstractions/IJavaScanner.cs
+++
@@ -1,8 +0,0 @@
-namespace PCL.CE.Neo.Core.Abstractions;
-
-public interface IJavaScanner
-{
-    IEnumerable<string> ScanJavaPaths();
-    IEnumerable<string> ScanDirectory(string directory);
-    bool IsValidJavaPath(string path);
-}
