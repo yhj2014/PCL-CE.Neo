3a74f7294edf2c11f2dd879b93c123263f63f86d
yhj2014 <18172759506@163.com>
2026-06-07 11:45:49 +0000

    feat: 检查0-3阶段进度
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- PCL-CE.Neo.App/App.xaml.cs
+++ PCL-CE.Neo.App/App.xaml.cs
@@ -1,38 +1,19 @@
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using PCL_CE.Neo.Core.Abstractions;
-using PCL_CE.Neo.Core;
-using PCL_CE.Neo.UI;
 
 namespace PCL_CE.Neo.App;
 
 public partial class App : Application
 {
-    public static IServiceProvider Services { get; private set; } = null!;
     private Window? _window;
+
+    public App()
+    {
+        InitializeComponent();
+    }
 
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
-        var services = new ServiceCollection();
-        ConfigureServices(services);
-        Services = services.BuildServiceProvider();
-
         _window = new MainWindow();
-        Window.ActiveWindow = _window;
         _window.Activate();
-    }
-
-    private static void ConfigureServices(IServiceCollection services)
-    {
-#if WINDOWS
-        services.AddWindowsPlatformServices();
-#elif MACOS
-        services.AddMacOSPlatformServices();
-#elif LINUX
-        services.AddLinuxPlatformServices();
-#endif
-
-        services.AddSingleton<MainViewModel>();
     }
 }
